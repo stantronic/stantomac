b555a7ce84fa38a6eb7ec2c0769355952150faf4
Stanton <stantronic@gmail.com>
2019-06-26 20:37:42 +0100

    Update the_whole_kaboodle.sh

--- the_whole_kaboodle.sh
+++ the_whole_kaboodle.sh
@@ -7,6 +7,7 @@ else
   echo $WHICH_XCODE
 fi
 
+# Install homebrew if not already installed
 WHICH_BREW=$(which brew)
 echo "$WHICH_BREW"
 if [[ $WHICH_BREW == "/usr/local/bin/brew"* ]]; then
@@ -15,7 +16,7 @@ else
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-
+# Install ansible if not already installed
 WHICH_ANSIBLE=$(which ansible)
 if [[ $WHICH_ANSIBLE == "/usr/local/bin/ansible" ]]; then
   echo "Ansible already installed at $WHICH_ANSIBLE"
